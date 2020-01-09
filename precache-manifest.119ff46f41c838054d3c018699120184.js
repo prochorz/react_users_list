e19b391bc615049af8d79478d0d991284196f329
prochor <prochorz@gmail.com>
2020-01-09 04:33:31 +0200

    Updates

---
+++ precache-manifest.119ff46f41c838054d3c018699120184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f249051e06f24b41a3d3c301683cd31e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8589af31b19bd7ac8bf",
+    "url": "/static/css/main.4de95741.chunk.css"
+  },
+  {
+    "revision": "81f2adac4aab614fb7a1",
+    "url": "/static/js/2.44186cf8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.44186cf8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8589af31b19bd7ac8bf",
+    "url": "/static/js/main.b678695b.chunk.js"
+  },
+  {
+    "revision": "8ffc8803dbe9ff207126",
+    "url": "/static/js/runtime-main.0c5ab20e.js"
+  }
+]);
